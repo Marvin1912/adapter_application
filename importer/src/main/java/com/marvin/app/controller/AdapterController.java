48fdab494692770d45d29cf36c01ceaba7f119fd
Marvin <geitnermarvin@gmail.com>
2025-11-23 15:20:45 +0100

    Some reworks

--- importer/src/main/java/com/marvin/app/controller/AdapterController.java
+++ importer/src/main/java/com/marvin/app/controller/AdapterController.java
@@ -1,6 +1,7 @@
 package com.marvin.app.controller;
 
 import com.marvin.app.importer.costs.CostImporter;
+import com.marvin.app.importer.sensors.SensorDataImporter;
 import com.marvin.export.Exporter;
 import com.marvin.upload.Uploader;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdapterController {
 
     private final CostImporter costImporter;
+    private final SensorDataImporter sensorDataImporter;
     private final Uploader uploader;
     private final Exporter exporter;
 
-    public AdapterController(CostImporter costImporter, Uploader uploader, Exporter exporter) {
+    public AdapterController(CostImporter costImporter, SensorDataImporter sensorDataImporter, Uploader uploader, Exporter exporter) {
         this.costImporter = costImporter;
+        this.sensorDataImporter = sensorDataImporter;
         this.uploader = uploader;
         this.exporter = exporter;
     }
@@ -23,6 +26,12 @@ public class AdapterController {
     @PostMapping("/import/costs")
     public ResponseEntity<Void> triggerCostImport() {
         costImporter.importFiles();
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/import/sensordata")
+    public ResponseEntity<Void> triggerSensordataImport() {
+        sensorDataImporter.importFiles();
         return ResponseEntity.ok().build();
     }
 
