807022b327756bbcff2f1fc6dd9c1df216df01f0
Marvin <geitnermarvin@gmail.com>
2026-04-03 11:59:52 +0200

    Updates

--- plants/src/main/java/com/marvin/plants/configuration/PlantSchedulerConfig.java
+++ plants/src/main/java/com/marvin/plants/configuration/PlantSchedulerConfig.java
@@ -2,7 +2,9 @@ package com.marvin.plants.configuration;
 
 import com.marvin.plants.service.PlantService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @Slf4j
@@ -15,14 +17,28 @@ public class PlantSchedulerConfig {
         this.plantService = plantService;
     }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void onStartup() {
+        executeWateringSchedule();
+        executeFertilizingSchedule();
+    }
+
     @Scheduled(cron = "0 0 0/4 * * *")
     public void scheduleWatering() {
-        log.info("Scheduling watering");
-        plantService.sendWateringNotification();
+        executeWateringSchedule();
     }
 
     @Scheduled(cron = "0 5 0 * * *")
     public void scheduleFertilizing() {
+        executeFertilizingSchedule();
+    }
+
+    private void executeWateringSchedule() {
+        log.info("Scheduling watering");
+        plantService.sendWateringNotification();
+    }
+
+    private void executeFertilizingSchedule() {
         log.info("Scheduling Fertilizing");
         plantService.sendFertilizingNotification();
     }
